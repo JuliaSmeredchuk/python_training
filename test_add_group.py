aeb54887f28ae3b88615137d1d215959f492bb10
Julia Smeredchuk <utah2402@gmail.com>
2018-07-23 01:09:12 +0400

    Записан рекордером сценарий

--- test_add_group.py
+++ test_add_group.py
@@ -12,7 +12,7 @@ def is_alert_present(wd):
 
 class test_add_group(unittest.TestCase):
     def setUp(self):
-        self.wd = WebDriver()
+        self.wd = WebDriver(capabilities={"marionette": False}, firefox_binary="C:/Program Files/Mozilla Firefox 52.9.0/firefox.exe")
         self.wd.implicitly_wait(60)
     
     def test_test_add_group(self):
@@ -22,7 +22,6 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_id("LoginForm").click()
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
