1270d70b159b176a5964c2d2ab841fc5efd70f7f
Julia Smeredchuk <utah2402@gmail.com>
2018-07-24 13:29:09 +0400

    Метод create_group перемещен наверх

--- test_add_group.py
+++ test_add_group.py
@@ -21,7 +21,6 @@ class test_add_group(unittest.TestCase):
         wd.get("http://localhost/addressbook/")
 
     def login(self, wd):
-        # login
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
@@ -29,23 +28,9 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-    
-    def test_test_add_group(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd)
-        self.open_groups_page(wd)
-        self.create_group(wd)
-        self.return_to_groups_page(wd)
-        self.logout(wd)
 
-    def logout(self, wd):
-        # logout
-        wd.find_element_by_link_text("Logout").click()
-
-    def return_to_groups_page(self, wd):
-        # return to groups page
-        wd.find_element_by_link_text("group page").click()
+    def open_groups_page(self, wd):
+        wd.find_element_by_link_text("groups").click()
 
     def create_group(self, wd):
         # init group creation
@@ -63,9 +48,26 @@ class test_add_group(unittest.TestCase):
         # submit group creation
         wd.find_element_by_name("submit").click()
 
-    def open_groups_page(self, wd):
-        # open groups page
-        wd.find_element_by_link_text("groups").click()
+    def test_test_add_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_groups_page(wd)
+        self.create_group(wd)
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        # logout
+        wd.find_element_by_link_text("Logout").click()
+
+    def return_to_groups_page(self, wd):
+        # return to groups page
+        wd.find_element_by_link_text("group page").click()
+
+
+
+
 
 
 
