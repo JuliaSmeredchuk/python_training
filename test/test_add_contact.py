3a1b23a55a30564dff2b37fdbc7bd79a32fe9947
Julia Smeredchuk <utah2402@gmail.com>
2018-07-31 10:10:07 +0400

    Выделен класс для управления сессиями

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -11,9 +11,9 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(firstname="Julia", middlename="Vladimirovna", lastname="Smeredchuk", nickname="-", title="fdte", company="sderty", address="asdfghj", home="qwerty",
                             mobile="89257789625", work="4512368", fax="789456", email="1@1.com", email2="1@1.com", email3="1@1.com", homepage="---", byear="1988", ayear="2018",
                             address2="bhgyfte", phone2="rteyuimn", notes="rtyuioppm"))
-    app.logout()
+    app.session.logout()
 
