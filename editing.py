dcf60879b14ba6aee79e55221f04f057e061adfa
Julia Smeredchuk <utah2402@gmail.com>
2018-08-02 12:19:30 +0400

    Выделены вспомогательные методы и классы для test_edit_contact

---
+++ editing.py
@@ -0,0 +1,8 @@
+
+class Editing:
+
+    def __init__(self, company, mobile, address2, notes):
+        self.company = company
+        self.mobile = mobile
+        self.address2 = address2
+        self.notes = notes
--- test_edit_contact.py
+++ test_edit_contact.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
 import unittest
+from editing import Editing
 
 def is_alert_present(wd):
     try:
@@ -17,32 +18,32 @@ class test_edit_contact(unittest.TestCase):
     def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/")
 
-    def login(self, wd):
+    def login(self, wd, username="admin", password="secret"):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
-    def edit_contact(self, wd):
+    def edit_contact(self, wd, editing):
         wd.find_element_by_xpath("//table[@id='maintable']/tbody/tr[8]/td[8]/a/img").click()
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
         wd.find_element_by_name("lastname").send_keys()
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys("asdfg")
+        wd.find_element_by_name("company").send_keys(editing.company)
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("89257788625")
+        wd.find_element_by_name("mobile").send_keys(editing.mobile)
         wd.find_element_by_name("address2").click()
         wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys("erty")
+        wd.find_element_by_name("address2").send_keys(editing.address2)
         wd.find_element_by_name("notes").click()
         wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys("5")
+        wd.find_element_by_name("notes").send_keys(editing.notes)
         # submit contact deletion
         wd.find_element_by_xpath("//div[@id='content']/form[1]/input[22]").click()
 
@@ -53,11 +54,11 @@ class test_edit_contact(unittest.TestCase):
         wd.find_element_by_link_text("Logout").click()
 
 
-    def test_test_edit_contact(self):
+    def test_edit_contact(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
-        self.edit_contact(wd)
+        self.login(wd, username="admin", password="secret")
+        self.edit_contact(wd, Editing(company="asdfg", mobile="89257788625", address2="erty", notes="5"))
         self.return_to_home_page(wd)
         self.logout(wd)
 
