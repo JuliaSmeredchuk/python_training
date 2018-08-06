4f83ea343de9e3ef27b5a8ce080fd3e59b073615
Julia Smeredchuk <utah2402@gmail.com>
2018-08-06 23:08:07 +0400

    Реализована валидация фикстуры перед каждым тестом

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
-from contact import Contact
+from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="Julia", middlename="Vladimirovna", lastname="Smeredchuk", nickname="-", title="fdte", company="sderty", address="asdfghj", home="qwerty",
                                mobile="89257789625", work="4512368", fax="789456", email="1@1.com", email2="1@1.com", email3="1@1.com", homepage="---", byear="1988", ayear="2018",
                                address2="bhgyfte", phone2="rteyuimn", notes="rtyuioppm"))
-    app.session.logout()
 
--- test/test_group_modification.py
+++ test/test_group_modification.py
@@ -2,7 +2,5 @@
 from model.group_modification import Modification
 
 def test_group_modification(app):
-    app.session.login(username="admin", password="secret")
     app.group.modification(Modification(name="1", header="1", footer="1"))
-    app.session.logout()
 
