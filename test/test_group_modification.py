15e6ba57dc85f682b49ab34ae57ecefada76d9f1
Julia Smeredchuk <utah2402@gmail.com>
2018-08-07 00:37:50 +0400

    Оптимизирован ввод данных для test_group_modification

--- test/test_group_modification.py
+++ test/test_group_modification.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
 from model.group_modification import Modification
+from model.group import Group
+
+
+def test_modify_group_name(app):
+    app.group.modify_first_group(Group(name="New group"))
+
+
+def test_modify_group_header(app):
+    app.group.modify_first_group(Group(header="New header"))
+
+
+def test_modify_group_footer(app):
+    app.group.modify_first_group(Group(footer="New footer"))
+
 
 def test_group_modification(app):
     app.group.modification(Modification(name="1", header="1", footer="1"))
-
