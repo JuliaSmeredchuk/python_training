9544f436615d678c5d7daa7653c175aa6c1924b2
Julia Smeredchuk <utah2402@gmail.com>
2018-08-08 00:26:04 +0400

    Добавлены предусловия в тесты для модификации групп

--- fixture/group.py
+++ fixture/group.py
@@ -31,27 +31,6 @@ class GroupHelper:
             wd.find_element_by_name(field_name).clear()
             wd.find_element_by_name(field_name).send_keys(text)
 
-    def modification(self, group_modification):
-        wd = self.app.wd
-        self.open_groups_page()
-        # init group modification
-        if not wd.find_element_by_name("selected[]").is_selected():
-            wd.find_element_by_name("selected[]").click()
-        wd.find_element_by_name("edit").click()
-        # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group_modification.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group_modification.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group_modification.footer)
-        # submit group modification
-        wd.find_element_by_name("update").click()
-        self.return_to_groups_page()
-
 
     def modify_first_group(self, new_group_data):
         wd = self.app.wd
--- test/test_group_modification.py
+++ test/test_group_modification.py
@@ -1,19 +1,26 @@
 # -*- coding: utf-8 -*-
-from model.group_modification import Modification
 from model.group import Group
 
 
 def test_modify_group_name(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.modify_first_group(Group(name="New group"))
 
 
 def test_modify_group_header(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.modify_first_group(Group(header="New header"))
 
 
 def test_modify_group_footer(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.modify_first_group(Group(footer="New footer"))
 
 
 def test_group_modification(app):
-    app.group.modification(Modification(name="1", header="1", footer="1"))
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
+    app.group.modify_first_group(Group(name="1", header="1", footer="1"))
