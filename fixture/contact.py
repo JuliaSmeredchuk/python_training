084519ff9bb2ee860b98946d97630a3f75b538c6
Julia Smeredchuk <utah2402@gmail.com>
2018-08-15 00:19:16 +0400

    Получение списка контактов

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,4 +1,6 @@
 
+from model.contact import Contact
+
 class ContactHelper:
 
     def __init__(self, app):
@@ -84,7 +86,22 @@ class ContactHelper:
 
     def count(self):
         wd = self.app.wd
+        self.check_contacts()
+        return len(wd.find_elements_by_name("selected[]"))
+
+    def check_contacts(self):
+        wd = self.app.wd
         if not (wd.current_url.endswith("/addressbook") and len(wd.find_elements_by_name("searchstring")) > 0):
             wd.find_element_by_link_text("home").click()
-        return len(wd.find_elements_by_name("selected[]"))
+
+    def get_contact_list(self):
+        wd = self.app.wd
+        self.check_contacts()
+        contacts = []
+        for element in wd.find_elements_by_tag_name("td.center"):
+            text = element.text
+            id = element.find_element_by_name("selected[]").get_attribute("value")
+            contacts.append(Contact(firstname=text, id=id))
+        return contacts
+
 
