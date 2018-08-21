e9e8ff761e104313c6f26acd794b9f529075a8f6
Julia Smeredchuk <utah2402@gmail.com>
2018-08-21 16:49:57 +0400

    Изменены тестовые данные для test_add_contact

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -4,9 +4,9 @@ from model.contact import Contact
 
 def test_add_contact(app):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="Julia", middlename="Vladimirovna", lastname="Smeredchuk", nickname="-", title="fdte", company="sderty", address="asdfghj", home="qwerty",
-                               mobile="89257789625", work="4512368", fax="789456", email="1@1.com", email2="1@1.com", email3="1@1.com", homepage="---", byear="1988", ayear="2018",
-                               address2="bhgyfte", phone2="rteyuimn", notes="rtyuioppm")
+    contact = Contact(firstname="Test1", middlename="Test", lastname="Test2", nickname="-", title="fdte", company="sderty", address="asdfghj", home="qwerty",
+                               mobile="654321", work="4512368", fax="789456", email="1@1.com", email2="1@1.com", email3="1@1.com", homepage="---", byear="1988", ayear="2018",
+                               address2="bhgyfte", phone2="852963", notes="rtyuioppm")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
