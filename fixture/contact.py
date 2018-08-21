07711a357e0a8b57b7f210999c9f3bc17dfb0455
Julia Smeredchuk <utah2402@gmail.com>
2018-08-21 23:11:48 +0400

    Реализован метод обратных проверок для электронной почты+добавлена рандомность выбора контакта

--- fixture/contact.py
+++ fixture/contact.py
@@ -125,7 +125,9 @@ class ContactHelper:
                 lastname = cells[1].text
                 id = cells[0].find_element_by_tag_name("input").get_attribute("value")
                 all_phones = cells[5].text
-                self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, id=id, all_phones_from_home_page=all_phones))
+                all_emails = cells[4].text
+                self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, id=id,
+                                                  all_phones_from_home_page=all_phones, all_emails_from_home_page=all_emails))
         return list(self.contact_cache)
 
     def open_contact_to_edit_by_index(self, index):
@@ -152,8 +154,12 @@ class ContactHelper:
         work = wd.find_element_by_name("work").get_attribute("value")
         mobile = wd.find_element_by_name("mobile").get_attribute("value")
         phone2 = wd.find_element_by_name("phone2").get_attribute("value")
+        email = wd.find_element_by_name("email").get_attribute("value")
+        email2 = wd.find_element_by_name("email2").get_attribute("value")
+        email3 = wd.find_element_by_name("email3").get_attribute("value")
         return Contact(firstname=firstname, lastname=lastname, id=id, home=home, work=work,
-                       mobile=mobile, phone2=phone2)
+                       mobile=mobile, phone2=phone2, email=email, email2=email2, email3=email3)
+
 
     def get_contact_from_view_page(self, index):
         wd = self.app.wd
