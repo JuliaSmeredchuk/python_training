24e54c49771681f3a59f653e03aa95b463b871b0
Julia Smeredchuk <utah2402@gmail.com>
2018-08-26 14:09:31 +0400

    Параметризованы тесты для создания групп и контактов: реализовано использование случайно сгенерированных строк для свойст создаваемых объектов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,12 +1,25 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
+
+def random_string(prefix, maxlen):
+    symbols = string. ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+testdata = [Contact(firstname="", middlename="", lastname="")] + [
+    Contact(firstname=random_string("firstname", 15), middlename=random_string("middlename", 20), lastname=random_string("lastname", 25),
+            nickname=random_string("nickname", 10), address=random_string("address", 10), home=random_string("home", 8),
+            mobile=random_string("mobile", 8), work=random_string("work", 8), email=random_string("email", 8),
+            email2=random_string("email2", 8), email3=random_string("email3", 8), phone2=random_string("phone2", 8))
+    for i in range (5)
+]
 
 
-def test_add_contact(app):
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="Test1", middlename="Test", lastname="Test2", nickname="-", title="fdte", company="sderty", address="asdfghj", home="qwerty",
-                               mobile="654321", work="4512368", fax="789456", email="1@1!.com", email2="1@1^.com", email3="1@1.com", homepage="---", byear="1988", ayear="2018",
-                               address2="bhgyfte", phone2="852963", notes="rtyuioppm")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
